eda84f38aaceede63804843a14126574ba739e10
Simion Dragos <paradox.dragos@yahoo.com>
2024-11-23 10:56:01 +0200

    first commit;

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-blue-500 text-center">Test</div>
+    </>
+  )
+}
+
+export default App
